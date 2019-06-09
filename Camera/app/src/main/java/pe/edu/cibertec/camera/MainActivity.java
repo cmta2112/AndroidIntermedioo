be241da17a3472c0c1d7f2bb0e2449093426fe74
cmta2112 <cmta2112@gmail.com>
2019-06-09 09:41:17 -0500

    pokemon v2

--- Camera/app/src/main/java/pe/edu/cibertec/camera/MainActivity.java
+++ Camera/app/src/main/java/pe/edu/cibertec/camera/MainActivity.java
@@ -4,12 +4,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,16 +25,17 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.SimpleFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btCamera, btGallery;
+    ImageView ivPhoto;
+
+
     static final int REQUEST_CAMERA = 1;
-    static final int REQUEST_TAKE_PICTURE = 2 ;
+    static final int REQUEST_TAKE_PICTURE = 2;
 
     String currentPathImage; // Ruta absoluta de la imagen
-    Button btCamera;
-    ImageView ivPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         btCamera = findViewById(R.id.btCamera);
         ivPhoto = findViewById(R.id.ivPhoto);
+        btGallery = findViewById(R.id.btGallery);
 
         btCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,88 +53,107 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                File file = new File(currentPathImage);
 
+                Uri contentUri = Uri.fromFile(file);
+                intent.setData(contentUri);
+                MainActivity.this.sendBroadcast(intent);
+            }
+        });
     }
+
 
     private void takePicture() {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        //Validar que la camara está disponible
-        if (cameraIntent.resolveActivity(getPackageManager())!= null){
+        // Validar que la cámara este disponible
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
 
-            // verificar que se disponga del permiso
+            // Verificar que se disponga del permiso
+
             if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED){
+                    != PackageManager.PERMISSION_GRANTED) {
 
                 requestPermission();
 
 
-            } else{
+            } else {
 
                 File photofile = null;
+
                 try {
-                    photofile = createImge();
+                    photofile = createImage();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-                startActivityForResult(cameraIntent,REQUEST_TAKE_PICTURE);
+                if (photofile!=null){
+                    Uri photoUri =
+                            FileProvider.getUriForFile(this, "pe.edu.cibertec.camera",photofile);
+
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+
+                    startActivityForResult(cameraIntent, REQUEST_TAKE_PICTURE);
+                }
+
+
+                //startActivityForResult(cameraIntent, REQUEST_TAKE_PICTURE);
             }
         }
 
     }
 
-    private File createImge() throws IOException {
-        // para crear el archivo , se genera primero un nombre,// pPARA CALCULAR FECHA Y AHORA DEL MOMENTO
+    private File createImage() throws IOException {
+
+        // Asignarle un nombre
 
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
 
-        //asignarle un directorio de almacenamiento
+        // Asignale un directorio de almacenamiento
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
-        //crear el archivo
+        // Crear el archivo
         File image = File.createTempFile(
                 imageFileName, // nombre
-                ".jpg",
-                storageDir) ;
+                ".jpg", // extensión
+                storageDir
+        );
 
         currentPathImage = image.getAbsolutePath();
         return image;
     }
 
-
-
     private void requestPermission() {
-        ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.CAMERA}, REQUEST_CAMERA);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}
+                , REQUEST_CAMERA);
     }
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent information) {
-        //super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_TAKE_PICTURE && resultCode == RESULT_OK){
-           // Glide.with(this).load(currentPathImage).into(ivPhoto);
+        Glide.with(this).load(currentPathImage).into(ivPhoto);
+      /*  if (requestCode == REQUEST_TAKE_PICTURE && resultCode == RESULT_OK) {
+            //Glide.with(this).load(currentPathImage).into(ivPhoto);
+
             Bitmap bitmap = (Bitmap) information.getExtras().get("data");
             ivPhoto.setImageBitmap(bitmap);
-        }
-
-
+        } */
     }
-    // PARA GUARDAR UNA FOTO SE TIENE Q CREAR UN ARCHIVO
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-       // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode ==  REQUEST_CAMERA){
-            if (//grantResults.length > 0 &&
-             grantResults[0] == PackageManager.PERMISSION_GRANTED){ // SABER SI ESTAN DANDO PERMISOS
-                Toast.makeText(MainActivity.this, "Se dio permiso", Toast.LENGTH_SHORT).show();
-
+        if (requestCode == REQUEST_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 takePicture();
-
             }
         }
+
     }
 }
---
+++ PokemonApiV2/app/src/main/java/pe/edu/cibertec/pokemonapiv2/AppDatabase.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.pokemonapiv2;
+
+public class AppDatabase {
+}
